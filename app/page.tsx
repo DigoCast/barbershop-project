857c5902ea8a8645f5bb1dcc5863043e29bf1f59
Diego <diegocastilholorenceto@gmail.com>
2025-05-10 20:50:41 -0300

    feat: add booking card at Home

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/Header"
 import { Button } from "./_components/ui/button"
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -10,7 +13,7 @@ const Home = () => {
       <Header />
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá, Diego!</h2>
-        <p>sabado, 10 de maio.</p>
+        <p>Sábado, 10 de maio.</p>
 
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
@@ -27,6 +30,29 @@ const Home = () => {
             className="object-cover rounded-xl h-auto w-auto"
           />
         </div>
+
+        <h2 className="uppercase text-gray-400 font-bold text-xs mt-6 mb-3">agendamentos</h2>
+        <Card className="">
+            <CardContent className="flex justify-between p-0">
+              <div className="flex flex-col gap-2 py-5 pl-5">
+                <Badge className="w-fit p-[6px] bg-[#221C3D] text-primary">Confirmado</Badge>
+                <h3 className="">Corte de cabelo</h3>
+                <div className="flex items-center gap-2">
+                  <Avatar>
+                    <AvatarImage src="https://utfs.io/f/178da6b6-6f9a-424a-be9d-a2feb476eb36-16t.png"/>
+                  </Avatar>
+                  <p>Barber Diego</p>
+                </div>
+              </div>
+              
+              <div className="flex flex-col justify-center items-center px-5 border-l-2 border-solid">
+                <p className="text-sm">Maio</p>
+                <p className="text-2xl">10</p>
+                <p className="text-sm">20:36</p>
+              </div>
+
+            </CardContent>
+        </Card>
       </div>
     </div>
   )
