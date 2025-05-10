cf39a84656cc12e365cd376488fec90c9431ebb6
Diego <diegocastilholorenceto@gmail.com>
2025-05-10 10:32:22 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "./_components/ui/button"
+
 const Home = () => {
-  return <h1 className="text-red-500">Home Page</h1>
+  return <Button>Botao</Button>
 }
 
 export default Home
