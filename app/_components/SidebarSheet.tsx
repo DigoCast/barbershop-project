b831b32af562017c0f0321fd5a1f54915adb2172
Diego <diegocastilholorenceto@gmail.com>
2025-05-16 19:54:53 -0300

    chore: add next-auth setup

--- app/_components/SidebarSheet.tsx
+++ app/_components/SidebarSheet.tsx
@@ -1,76 +1,102 @@
 import React from "react"
 import { Button } from "./ui/button"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
-import {
-  SheetClose,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-} from "./ui/sheet"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
+import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import quickSearchOptions from "../_constants/quickSearch"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
-    
-      <SheetContent className="overflow-y-auto">
-        <SheetHeader>
-          <SheetTitle className="text-left">Menu</SheetTitle>
-        </SheetHeader>
+    <SheetContent className="overflow-y-auto">
+      <SheetHeader>
+        <SheetTitle className="text-left">Menu</SheetTitle>
+      </SheetHeader>
 
-        <div className="flex items-center gap-3 border-b border-solid py-5">
-          <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Ola, Faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[80%] rounded">
+            <DialogHeader>
+              <DialogTitle>Faça seu login na plataforma!</DialogTitle>
+              <DialogDescription>
+                Conecte-se utilizando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant='outline' className="gap-1 font-bold">
+              <Image
+                alt="Google Login"
+                src="/google.svg"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
             <AvatarImage src="https://plus.unsplash.com/premium_vector-1728555238583-1e5258caabed?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTN8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D" />
           </Avatar>
           <div>
             <p className="font-bold">Diego Castilho</p>
             <p className="text-xs">diegocastilholorenceto@gmail.com</p>
-          </div>
-        </div>
+          </div> */}
+      </div>
 
-        <div className="flex flex-col gap-2 border-b border-solid py-5">
-          <SheetClose asChild>
-            <Button className="justify-start gap-2" variant="ghost" asChild>
-              <Link href="/">
-                <HomeIcon size={18} />
-                Início
-              </Link>
-            </Button>
-          </SheetClose>
-
-          <Button className="justify-start gap-2" variant="ghost">
-            <CalendarIcon size={18} />
-            Agendamentos
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
+        <SheetClose asChild>
+          <Button className="justify-start gap-2" variant="ghost" asChild>
+            <Link href="/">
+              <HomeIcon size={18} />
+              Início
+            </Link>
           </Button>
-        </div>
+        </SheetClose>
 
-        <div className="flex flex-col gap-2 border-b border-solid py-5">
-          {quickSearchOptions.map((option) => (
-            <Button
-              className="justify-start gap-2"
-              variant="ghost"
-              key={option.title}
-            >
-              <Image
-                alt={option.title}
-                src={option.imageUrl}
-                width={18}
-                height={18}
-              />
-              {option.title}
-            </Button>
-          ))}
-        </div>
+        <Button className="justify-start gap-2" variant="ghost">
+          <CalendarIcon size={18} />
+          Agendamentos
+        </Button>
+      </div>
 
-        <div className="flex flex-col gap-2 py-5">
-          <Button variant="ghost" className="justify-start">
-            <LogOutIcon size={18} />
-            Sair da conta
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
+        {quickSearchOptions.map((option) => (
+          <Button
+            className="justify-start gap-2"
+            variant="ghost"
+            key={option.title}
+          >
+            <Image
+              alt={option.title}
+              src={option.imageUrl}
+              width={18}
+              height={18}
+            />
+            {option.title}
           </Button>
-        </div>
-      </SheetContent>
+        ))}
+      </div>
+
+      <div className="flex flex-col gap-2 py-5">
+        <Button variant="ghost" className="justify-start">
+          <LogOutIcon size={18} />
+          Sair da conta
+        </Button>
+      </div>
+    </SheetContent>
   )
 }
 
