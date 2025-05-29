5f3c0594f5c8aa52320185e38630f5468647384a
Diego <diegocastilholorenceto@gmail.com>
2025-05-29 14:37:21 -0300

    fix: fix serviceItem imports

--- app/_actions/CreateBooking.ts
+++ app/_actions/CreateBooking.ts
@@ -16,7 +16,7 @@ export const createBooking = async(params: CreateBookingParams) => {
         throw new Error("Usuario não logado")
     }
     await db.booking.create({
-        data: {...params, userId: (user.user as any).id}, 
+        data: {...params, userId: (user.user as any).id}, // eslint-disable-line
     }) 
     revalidatePath("/barbershops/[id]")
     revalidatePath("/bookings")
